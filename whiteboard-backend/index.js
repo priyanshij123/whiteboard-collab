5c83ae0832ec53ef9939725c8da8a3f8cc65e0a0
priyanshij123 <148970696+priyanshij123@users.noreply.github.com>
2025-06-23 15:44:35 +0530

    Update index.js

--- whiteboard-backend/index.js
+++ whiteboard-backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://whiteboard-collab-psi.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
